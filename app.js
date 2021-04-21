e411582a9a7510fa4e536559ed3d83af0ca68468
Rafael <raframos@deloitte.com>
2021-04-21 11:45:59 -0300

    metodos dos botoes do jogo

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 new Vue({
     el:'#app',
     data:{
+        running : false,
         playerLife: 100,
         monsterLife: 100,
+        logs: [],
     },
     computed: {
         hasResult() {
@@ -10,9 +12,40 @@ new Vue({
         }
     },
     methods: {
-        
+        startGame(){
+            this.running = true,
+            this.playerLife = 100,
+            this.monsterLife = 100
+        },
+        attack(special){
+            this.hurt('monsterLife', 5, 10, special)
+            this.hurt('playerLife', 7, 12, false)
+        },
+        hurt(prop, min, max, special){
+            const plus = special ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[prop] = Math.max(this[prop] - hurt, 0)
+        },
+        healAndHurt(){
+            this.heal(10, 15)
+            this.hurt('playerLife', 7, 12, false)
+        },
+        heal(min, max){
+               const heal = this.getRandom(min,max)
+               this.playerLife = Math.min(this.playerLife + heal, 100) 
+        },
+        getRandom(min, max){
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        },
+        registerLog(text, cls){
+            this.logs.unshift({text, cls});
+        }
     },
     watch: {
-
+        hasResult(value){
+            if(value) 
+               this.running = false 
+        }
     }
 })
